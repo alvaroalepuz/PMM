cb343fcaead89da965c4f1a3fc7a8e5d5b8ff3ab
alvaroalepuz <alvaroalepuz99@gmail.com>
2020-09-24 18:21:58 +0200

    Ejercicio2.1

---
+++ main.js
@@ -0,0 +1,6 @@
+var mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+for (let i = 0; i < mes.length; i++) 
+{
+    console.log(mes[i] + "\n");
+}
