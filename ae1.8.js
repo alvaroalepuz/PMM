ee591946014dba96a3d303062eeda46c89e541be
alvaroalepuz <alvaroalepuz99@gmail.com>
2020-09-24 20:18:01 +0200

    Actividad aev1.8

---
+++ ae1.8.js
@@ -0,0 +1,19 @@
+var introducido=prompt("Introduce un numero");
+
+
+
+
+    function dado(introducido) 
+        {
+            var numero = Math.floor(Math.random() * 12) + 1;
+
+            if (introducido=!numero) {
+                
+            } else {
+                
+            }
+            
+        {
+
+    }
+}
