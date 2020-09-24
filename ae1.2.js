f81a330f158d2ddc1c59586ff5e150253ab20010
alvaroalepuz <alvaroalepuz99@gmail.com>
2020-09-24 18:28:14 +0200

    Cambio de nombre

--- ae1.2.js
+++ ae1.2.js
@@ -1,6 +1,3 @@
 var mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-for (let i = 0; i < mes.length; i++) 
-{
-    console.log(mes[i] + "\n");
-}
+    console.table(mes);
