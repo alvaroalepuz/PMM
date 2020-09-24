4bfae59fcb89fc6502d5d4b2016db1d665231696
alvaroalepuz <alvaroalepuz99@gmail.com>
2020-09-24 16:54:24 +0200

    Finalizado ejercicio 1

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hola mundo!");
+let alertmessage="Soy el primer script";
+const ALERT_MESSAGE="Soy el primer script";
+window.alert(alertmessage);
+window.alert("Soy el primer script");
+window.alert(ALERT_MESSAGE+"y estoy funcionando sobre"+ navigator.appVersion);
