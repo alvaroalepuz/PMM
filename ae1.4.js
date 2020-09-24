24d40ab72011d81bae28295f5a068ff05bc73712
alvaroalepuz <alvaroalepuz99@gmail.com>
2020-09-24 18:54:45 +0200

    Finalizado el ae1.4

---
+++ ae1.4.js
@@ -0,0 +1,15 @@
+Var numero1 = 5;
+Var numero2 = 8;
+
+If(numero1 < numero2){
+ console.log(“numero1 no es mayor que numero2”);
+}
+If(numero2 > 0){
+ console.log(“numero2 es positivo”);
+}
+If(numero1 ¡0){
+ console.log(“numero1 es negativo o distinto de cero”);
+}
+If(numero1 +1 <= numero2){
+ Console.log(“Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2”);
+}
